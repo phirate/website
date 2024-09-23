4a6670eb0abdd2d567ad6f34f9a7c37aa165a285
pihedron <pihedron@gmail.com>
2024-09-23 01:08:12 +0000

    added courses route

---
+++ src/routes/courses/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  //
+</script>
+
+<section id="banner">
+  <span id="pill">Courses</span>
+  <h1>Interactive Study Material</h1>
+</section>
+
+<style>
+  #banner {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+  }
+
+  #pill {
+    padding: 4px 16px;
+    border: 2px solid var(--color);
+    border-radius: 100vmax;
+    text-transform: uppercase;
+    font-size: 16px;
+  }
+
+  #banner h1 {
+    margin-block: 0 0;
+  }
+</style>
