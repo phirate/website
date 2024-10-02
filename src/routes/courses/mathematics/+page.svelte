c8638afa9d1b6e2ae1566e53adc24a1c48f5f050
pihedron <pihedron@gmail.com>
2024-10-02 02:26:53 +0000

    changed som

---
+++ src/routes/courses/mathematics/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import { fly } from 'svelte/transition'
+
+  let ready = false
+  onMount(() => (ready = true))
+</script>
+
+<section class="auto top">
+  {#if ready}
+    <h1 transition:fly={{ y: -32, duration: 1000, delay: 500 }}>Mathematics</h1>
+  {/if}
+</section>
