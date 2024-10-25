b72dfc8ebec6784bd3c696e30bf6d30594295719
pihedron <pihedron@gmail.com>
2024-10-25 00:10:00 +0000

    animated join page

--- src/routes/join/+page.svelte
+++ src/routes/join/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import Button from '$lib/components/Button.svelte'
   import { user } from '$lib/user'
-  import { fade, slide } from 'svelte/transition'
+  import { onMount } from 'svelte'
+  import { fly, slide } from 'svelte/transition'
 
   const login = async (e: SubmitEvent) => {
     e.preventDefault()
@@ -28,63 +29,68 @@
   let email = ''
   let password = ''
   let checked = false
+
+  let ready = false
+  onMount(() => (ready = true))
 </script>
 
 <section id="banner">
-  <h1>Join</h1>
-  <form on:submit={submit}>
-    {#if !checked}
-      <div class="group" transition:slide>
-        <input
-          id="name"
-          type="text"
-          name="name"
-          required
-          bind:value={name}
-          class:filled={name.length > 0}
-        />
-        <label for="name">Name</label>
-      </div>
-    {/if}
-    <div class="group">
-      <input
-        id="email"
-        type="text"
-        name="email"
-        required
-        bind:value={email}
-        class:filled={email.length > 0}
-      />
-      <label for="email">Email</label>
-    </div>
-    <div class="group">
-      <input
-        id="password"
-        type="password"
-        name="password"
-        required
-        bind:value={password}
-        class:filled={password.length > 0}
-      />
-      <label for="password">Password</label>
-    </div>
-    <div class="group">
-      <input type="checkbox" name="cb" id="cb" bind:checked />
-      <label for="cb" id="checkbox">
-        <svg viewBox="0 0 100 100">
-          <path
-            class="box"
-            d="M82,89H18c-3.87,0-7-3.13-7-7V18c0-3.87,3.13-7,7-7h64c3.87,0,7,3.13,7,7v64C89,85.87,85.87,89,82,89z"
+  {#if ready}
+    <h1 transition:fly={{ y: -32, duration: 1000 }}>Join</h1>
+    <form on:submit={submit} transition:fly={{ y: 32, duration: 1000 }}>
+      {#if !checked}
+        <div class="group" transition:slide>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            required
+            bind:value={name}
+            class:filled={name.length > 0}
           />
-          <polyline class="check" points="25.5,53.5 39.5,67.5 72.5,34.5 " />
-        </svg>
-        <span>I already have an account!</span>
-      </label>
-    </div>
-    <div class="group">
-      <Button type="submit">{checked ? 'Login' : 'Register'}</Button>
-    </div>
-  </form>
+          <label for="name">Name</label>
+        </div>
+      {/if}
+      <div class="group">
+        <input
+          id="email"
+          type="text"
+          name="email"
+          required
+          bind:value={email}
+          class:filled={email.length > 0}
+        />
+        <label for="email">Email</label>
+      </div>
+      <div class="group">
+        <input
+          id="password"
+          type="password"
+          name="password"
+          required
+          bind:value={password}
+          class:filled={password.length > 0}
+        />
+        <label for="password">Password</label>
+      </div>
+      <div class="group">
+        <input type="checkbox" name="cb" id="cb" bind:checked />
+        <label for="cb" id="checkbox">
+          <svg viewBox="0 0 100 100">
+            <path
+              class="box"
+              d="M82,89H18c-3.87,0-7-3.13-7-7V18c0-3.87,3.13-7,7-7h64c3.87,0,7,3.13,7,7v64C89,85.87,85.87,89,82,89z"
+            />
+            <polyline class="check" points="25.5,53.5 39.5,67.5 72.5,34.5 " />
+          </svg>
+          <span>I already have an account!</span>
+        </label>
+      </div>
+      <div class="group">
+        <Button type="submit">{checked ? 'Login' : 'Register'}</Button>
+      </div>
+    </form>
+  {/if}
 </section>
 
 <style>
